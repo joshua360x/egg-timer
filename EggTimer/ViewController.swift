e9bcf62c8ae73db29e77d9cb826237cbc7cbb94d
Josh Williams <joshua360x@gmail.com>
2023-05-08 15:22:30 -0700

    added egg selection with attributes

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let softTime = 5
+    let mediumTime = 7
+    let hardTime = 12
+    
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+//        print("This is the type of egg : \(sender.currentTitle)")
+        let hardness = sender.currentTitle
+        if hardness == "Soft" {
+            print("Cook time should be: \(softTime)")
+        }
+        if hardness == "Medium" {
+              print("Cook time should be: \(mediumTime)")
+          }
+        if hardness == "Hard" {
+              print("Cook time should be: \(hardTime)")
+          }
+        
+    }
+    
+    func typeOfEgg() {
+        
+    }
+    
 }
