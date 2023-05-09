14a062d2fdcbb868187a09ecdc0d948e5ff9cfcd
Josh Williams <joshua360x@gmail.com>
2023-05-08 18:50:29 -0700

    added functional progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -17,13 +17,17 @@ class ViewController: UIViewController {
     @IBOutlet weak var mainTitle: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     // dicttionary with times in seconds
-    let eggTimes = ["Soft": 5, "Medium": 420, "Hard": 720]
-    
+    let eggTimes = ["Soft": 3, "Medium": 420, "Hard": 720]
+//    var totalTime = 0
+    var secondsPassed : Float = 0.0
     var timer = Timer()
+    
+
+  
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
 //        print(mainTitle.text)
-        print(progressBar.progress)
+//        print(progressBar.progress)
 //        print("This is the type of egg : \(sender.currentTitle)")
         let hardness = sender.currentTitle!
 //        if hardness == "Soft" {
@@ -36,37 +40,53 @@ class ViewController: UIViewController {
 //              print("Cook time should be: \(hardTime)")
 //          }
 //        var timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(UIMenuController.update), userInfo: nil, repeats: true)
-        if hardness == "Soft" {
-            progressBar.progress = 1.0
-        }
+
         
-        print(progressBar.progress)
+//        print(progressBar.progress)
 
-
-        update(cookTime: eggTimes[hardness]!)
+        
+        update(cookTime: Float(eggTimes[hardness]!))
 
         // one line solution
 //        print(eggTimes[hardness]!)
     }
     
-    func update( cookTime: Int) {
-        var count = 0
-         count = cookTime
+    func update( cookTime: Float) {
+//        var count = 0
+//         count = cookTime
 
         
        
 
         timer.invalidate()
+//        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {  (Timer) in
+//            if count > 0 {
+//                print ("\(count) seconds")
+//                count -= 1
+//            } else {
+//                Timer.invalidate()
+//                self.mainTitle.text = "DONE!"
+//
+//            }
+//        }
+        
+        
         timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) {  (Timer) in
-            if count > 0 {
-                print ("\(count) seconds")
-                count -= 1
+            if self.secondsPassed < cookTime {
+                print ("\(self.secondsPassed) seconds")
+                var percentageProgress = self.secondsPassed / cookTime
+                print(percentageProgress)
+                self.progressBar.progress = Float(percentageProgress)
+                print("this is the number after the float: \(Float(percentageProgress))")
+                self.secondsPassed += 1
             } else {
                 Timer.invalidate()
+                self.progressBar.progress = 1.0
                 self.mainTitle.text = "DONE!"
                
             }
         }
+        
 //        print(count)
     }
      
